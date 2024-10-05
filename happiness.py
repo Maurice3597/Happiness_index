e55d8b9fae865bbf6c3fe2e251c01b5342bd85c4
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-10-05 19:58:21 +0100

    Added comments for each block of code

--- happiness.py
+++ happiness.py
@@ -2,20 +2,14 @@ import pandas
 import streamlit as st
 import plotly.express as px
 
+# Load the file containing the data (hppy.csv
 data = pandas.read_csv("happy.csv")
 
-
-# happiness = list(data["happiness"])
-# gdp = list(data["gdp"])
-# social_support = list(data["social_support"])
-# life_expectancy = list(data["life_expectancy"])
-# freedom_to_make_life_choices = list(data["freedom_to_make_life_choices"])
-# generosity =list(data["generosity"])
-# corruption = list(data["corruption"])
-
+# list of the column headers
 coordinate = list(data.columns)
 coordinate = coordinate[1:];print(coordinate)
 
+# Titles and Select boxes for variable options
 st.title("In Search For Happiness")
 Y_axis = st.selectbox("Select the Variable for the Vertical Axis: ", options=coordinate,
              placeholder="Select a variable for the Y_axis",
@@ -24,5 +18,8 @@ X_axis = st.selectbox("Select the Variable for the horizontal Axis: ", options=c
              placeholder="Select a variable for the X_axis",
              help="Choose the variable that represents the V_axis")
 
+# Assign the values of the select boxes to variables X and Y
 X, Y = list(data[X_axis]), list(data[Y_axis])
+
+# Plots and displays a plot of the selected variables
 st.plotly_chart(px.scatter(x=X, y=Y, labels={'x':X_axis, 'y':Y_axis}))
