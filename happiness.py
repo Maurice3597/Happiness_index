cb4799f45be96f43668fb739e9dd9deeb9b44243
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-10-05 18:56:15 +0100

    created a list of all the available colum  headers

--- happiness.py
+++ happiness.py
@@ -5,10 +5,15 @@ import plotly.express as px
 data = pandas.read_csv("happy.csv")
 
 
-happiness = list(data["happiness"])
-gdp = list(data["gdp"])
-social_support = list(data["social_support"])
-life_expectancy = list(data["life_expectancy"])
-freedom_to_make_life_choices = list(data["freedom_to_make_life_choices"])
-generosity =list(data["generosity"])
-corruption = list(data["corruption"])
+# happiness = list(data["happiness"])
+# gdp = list(data["gdp"])
+# social_support = list(data["social_support"])
+# life_expectancy = list(data["life_expectancy"])
+# freedom_to_make_life_choices = list(data["freedom_to_make_life_choices"])
+# generosity =list(data["generosity"])
+# corruption = list(data["corruption"])
+
+coordinate = list(data.columns);print(coordinate)
+print("Hello world")
+coordinate = coordinate[1:];print(coordinate)
+
