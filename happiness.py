b29d7b956d4d7acaf0c7a71e1a71337357e024d6
Maurice Tchouncha <datascienceandaienegneer@gmail.com>
2024-10-05 18:53:10 +0100

    Import necessary libaries and loading the data file

--- happiness.py
+++ happiness.py
@@ -1,3 +1,14 @@
 import pandas
 import streamlit as st
 import plotly.express as px
+
+data = pandas.read_csv("happy.csv")
+
+
+happiness = list(data["happiness"])
+gdp = list(data["gdp"])
+social_support = list(data["social_support"])
+life_expectancy = list(data["life_expectancy"])
+freedom_to_make_life_choices = list(data["freedom_to_make_life_choices"])
+generosity =list(data["generosity"])
+corruption = list(data["corruption"])
